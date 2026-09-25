678d4732c3b41df737415773144cbf5285e373d0
Michal Simo <michalsimo9@gmail.com>
2026-09-25 18:18:10 +0200

    Uloha 1

---
+++ 01_create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(20) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    region VARCHAR(50)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(20) PRIMARY KEY,
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(20) PRIMARY KEY,
+    customer_id VARCHAR(20) REFERENCES customers(customer_id),
+    product_id VARCHAR(20) REFERENCES products(product_id),
+    order_date DATE,
+    ship_date DATE,
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(10,2),
+    profit DECIMAL(10,2)
+);
